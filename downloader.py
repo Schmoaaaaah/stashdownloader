8cb41c1d04ff0ce18412260511993829d7be2110
Noah Kempers <nkempers@r4nd0.de>
2022-11-19 18:58:09 +0100

    phprofile

--- downloader.py
+++ downloader.py
@@ -10,17 +10,102 @@ stash = StashInterface({
     "Port": os.environ.get("STASH_PORT"),
 })
 
-ydl_opts = {
+stashstar = StashInterface({
+    "Scheme": "http",
+    "Host": os.environ.get("STASH_STAR_DOMAIN"),
+    "Port": os.environ.get("STASH_STAR_PORT"),
+})
+
+ydl_opts_normal = {
     'outtmpl': '/media/%(webpage_url_domain)s/%(title)s.%(ext)s',
     'external_downloader': 'aria2c',
     'writeinfojson': True,
     'quiet': True,
 }
+ydl_opts_phprofile = {
+    'outtmpl': '/media/'+os.environ.get("STASH_STAR_LIB")+'/%(webpage_url_domain)s/%(uploader)s/%(title)s.%(ext)s',
+    'external_downloader': 'aria2c',
+    'writeinfojson': True,
+    'quiet': True,
+    'max-downloads': 1,
+}
 mediapath = os.environ.get("STASH_MEDIA_PATH")
+
+
+def test(urls):
+    for line in urls:
+        with YoutubeDL(ydl_opts_phprofile) as ydl:
+            info = ydl.extract_info(line, download=False)
+            perfid = stashstar.findPerformerIdWithName(
+                info['entries'][0]['uploader'])
+            if perfid == None:
+                perfid = stashstar.createPerformerByName(
+                    info['entries'][0]['uploader'])
+                log.LogInfo(
+                    "Performer: "+info['entries'][0]['uploader']+" created with ID: "+perfid)
+            else:
+                log.LogInfo(
+                    "Performer: "+info['entries'][0]['uploader']+" found with ID: "+perfid)
+            return perfid
+
+
+def phprofile(urls):
+    for line in urls:
+        with YoutubeDL(ydl_opts_phprofile) as ydl:
+            info = ydl.extract_info(line, download=False)
+            perfid = stashstar.findPerformerIdWithName(
+                info['entries'][0]['uploader'])
+            if perfid == None:
+                perfid = stashstar.createPerformerByName(
+                    info['entries'][0]['uploader'])
+                log.LogInfo(
+                    "Performer: "+info['entries'][0]['uploader']+" created with ID: "+perfid)
+            else:
+                log.LogInfo(
+                    "Performer: "+info['entries'][0]['uploader']+" found with ID: "+perfid)
+            for video in info['entries']:
+                error_code = ydl.download(video['webpage_url'])
+                if(error_code != 0):
+                    log.LogError(
+                        "Download of Video: "+video['title']+" from profile "+video['uploader']+" failed ")
+                else:
+                    log.LogInfo(
+                        "Download of Video: "+video['title']+" from profile "+video['uploader']+" successful")
+            stashpath = mediapath + "stars/" + \
+                info['entries'][0]['webpage_url_domain'] + "/" + \
+                info['entries'][0]['uploader']+"/"
+            stashstar.metadata_scan(stashpath)
+            log.LogInfo("waiting 3 Minutes")
+            sleep(int(os.environ.get("STASH_SCAN_TIMEOUT")))
+            scenes = stashstar.findScenesByPathRegex(
+                r'.*\.(?:[mM][pP]4|[wW][mM][vV])$')
+            for video in info['entries']:
+                for scene in scenes:
+                    namefrompath = scene['path'].split("/")[-1]
+                    log.LogDebug("Name from Path: "+namefrompath +
+                                 "\nName from Video: "+video['title']+"."+video['ext'])
+                    if scene['title'] == video['title']+"."+video['ext'] or video['title']+"."+video['ext'] == namefrompath:
+                        log.LogInfo("Found Scne with ID: " +
+                                    scene['id']+" and Title: "+scene['title'])
+                        updatescene = {}
+                        updatescene['id'] = scene['id']
+                        updatescene['url'] = video['webpage_url']
+                        updatescene['title'] = video['title']
+                        updatescene['tag_ids'] = [
+                            stashstar.findTagIdWithName("scrape")]
+                        updatescene['performer_ids'] = perfid
+                        if scene.get('rating'):
+                            updatescene['rating'] = scene.get('rating')
+                        log.LogInfo("Updating Scene with ID: " +
+                                    updatescene['id']+" with video: "+video['title'])
+                        stashstar.updateScene(updatescene)
+
+    return scenes
+
 
 def videoslist(urls):
     for line in urls:
-        with YoutubeDL(ydl_opts) as ydl:
+        with YoutubeDL(ydl_opts_normal) as ydl:
             info = ydl.extract_info(line, download=False)
             log.LogInfo("Identified Video as: "+info.get('title'))
             error_code = ydl.download(line)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import json
-from downloader import videoslist
+from downloader import videoslist, phprofile, test
 from os import environ
 
 mode = environ.get("DEBUG")
@@ -9,12 +9,23 @@ app = Flask(__name__)
 
 
 @app.route('/videourllist', methods=['PUT'])
-def create_record():
+def videolistdownloader():
     record = json.loads(request.data)
     if record.get('method') == 'videos':
         videoslist(record.get('urls'))
         return jsonify({"status":"OK","message":"Videos added to Stash"})
     else:
         return jsonify({"status":"Error"})
+    
+
+@app.route('/phprofile', methods=['PUT'])
+def phprofiledownloader():
+    record = json.loads(request.data)
+    if record.get('method') == 'phprofile':
+        #info = test(record.get('urls'))
+        info = phprofile(record.get('urls'))
+        return jsonify(info)
+    else:
+        return jsonify({"status": "Error"})
 
 app.run(debug=mode, host='0.0.0.0')
